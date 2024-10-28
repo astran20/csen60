cf1575d4c4386c1957b07a6fde7862ebbbf60514
Andrew Tran <andtran20@gmail.com>
2024-10-28 11:35:12 -0700

    create addTodo function

--- Week6/javaScript/index.js
+++ Week6/javaScript/index.js
@@ -1,66 +1,23 @@
-// console.log("Hello World");
-alert('Hello World');
+const input = document.getElementById("list-input");
+console.log(input.value);
 
-window.document.write('<p>Welcome to CSEN 60! </p>');
-
-// document.append('<p>This should come after the h1</p>');
-
-/*
-  Function to calculate the sum of two numbers.
-  Inputs: a, b - numbers to be added.
-  Output: returns the sum of a and b.
-*/
-
-// TODO: fix this function
 
 /**
- * @description -- this function adds two numbers
- * @param {*} a 
- * @param {*} b 
- * @returns 
+ * takes input and adds to list
  */
-function add(a, b) {
-    return a + b;
+function addTodo(){
+  console.log("button was clicked");
+  const input = document.getElementById("list-input");
+  console.log("Input value =", input.value);
+
+  const ul = document.querySelector("ul");
+  // const list = lists[0];
+  console.log(ul);
+
+  const li = document.createElement("li");
+  li.textContent = input.value;
+  ul.appendChild(li);
 }
 
 
-let x = 100;
-/*let x as definition*/
-/* x=2 -> initialization*/
-if (true) {
-    let x = 200; // Same variable!
-}
-console.log(x); // Outputs: 200
 
-let y;
-y = null;
-let z = 0;
-console.log(y,z);
-
-let greeting = "Hello, world!";
-let response = `The time is ${new Date()}`;
-console.log(response);
-
-const age = 20;
-
-if(age>18){
-  console.log("You are of age");
-}else{
-  console.log(`You aren't of age. Please wait ${18-age} years`);
-}
-
-let person = {
-  name: "John",
-  age: 30,
-  isStudent: false
-};
-
-console.log(person.age);
-
-console.log(typeof(Hello World));
-
-// Strict equality -- 1. are these the same type? 2. are these the same value?
-console.log(3 === '3'); // false
-
-// Loose equality -- are these the same value?
-console.log(3 == '3'); // true
